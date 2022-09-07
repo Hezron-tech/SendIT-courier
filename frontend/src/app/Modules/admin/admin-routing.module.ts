ab4f04f372aba3cd2b4bbc8a5ea70cccf6110f11
Hezron-tech <kiprophez@gmail.com>
2022-09-07 10:46:28 +0300

    working update form

--- frontend/src/app/Modules/admin/admin-routing.module.ts
+++ frontend/src/app/Modules/admin/admin-routing.module.ts
@@ -1,3 +1,5 @@
+import { UpdateOrderComponent } from './update-order/update-order.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
 import { AllOrdersComponent } from './all-orders/all-orders.component';
 import { CreateOrdersComponent } from './create-orders/create-orders.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +7,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'create', component:CreateOrdersComponent},
-  {path:'all', component:AllOrdersComponent}
+  {path:'all', component:AllOrdersComponent},
+  {path:'view',component:ViewOrderComponent},
+  {path: 'update',component:UpdateOrderComponent}
 ];
 
 @NgModule({
--- frontend/src/app/Modules/admin/admin.module.ts
+++ frontend/src/app/Modules/admin/admin.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CreateOrdersComponent } from './create-orders/create-orders.component';
 import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+import { UpdateOrderComponent } from './update-order/update-order.component';
 
 
 @NgModule({
   declarations: [
     CreateOrdersComponent,
-    AllOrdersComponent
+    AllOrdersComponent,
+    ViewOrderComponent,
+    UpdateOrderComponent
   ],
   imports: [
     CommonModule,
