cd8ba8c4c13297802ee1ba605f9a74d67578dc42
Hezron-tech <kiprophez@gmail.com>
2022-09-19 09:53:59 +0300

    procedure

--- background-services/src/EmailService/registration.ts
+++ background-services/src/EmailService/registration.ts
@@ -17,68 +17,6 @@ interface Role{
     welcome:string
     
 }
-
-
-// const SendEmails= async()=>{
-// const pool = await mssql.connect(sqlConfig)
-// const users: role[] = await (await db.exec("welcomeEmail")).recordset;
-// const Roles:Role[]= await(await pool.request().query(`
-//  SELECT * FROM users WHERE welcome='0'`)).recordset
-
-//  for(let Role of Roles){
-
-//     ejs.renderFile('templates/registration.ejs',{username:Role.username,Role:Role.role} ,async(error,data)=>{
-
-//         let messageoption={
-//             from:process.env.EMAIL,
-//             to:Role.email,
-//             subject:" account registration",
-//             html:data,
-//             attachments:[
-//                 {
-//                     fileusername:'Role.txt',
-                    
-//                 }
-//             ]
-//         }
-
-//         try {
-            
-//             await sendMail(messageoption)
-//              await pool.request().query(`UPDATE users SET welcome='1' WHERE id = '${Role.id}'`)
-//             console.log('Email is Sent');
-            
-//         } catch (error) {
-//             console.log(error);
-            
-//         }
-
-
-//     })
-
-//  }
-
-
-// }
-
-// export default SendEmails
-
-
-// import ejs from "ejs";
-// import mssql from "mssql";
-// import dotenv from "dotenv";
-// import { sqlConfig } from "../Config/Config";
-// dotenv.config();
-// import sendMail from "../Helpers/Email";
-// import Connection from "../DatabaseHelpers/db";
-// const db = new Connection();
-// interface IUser {
-//   id: string;
-//   name: string;
-//   email: string;
-//   welcome: string;
-// }
-
 const SendEmails = async () => {
   const pool = await mssql.connect(sqlConfig);
   const users: Role[] = await (await db.exec("welcome")).recordset;
